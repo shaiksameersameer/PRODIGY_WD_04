44f69d06e9b2d44062adf1c022359bf0724e3d05
Sameer <152957348+shaiksameersameer@users.noreply.github.com>
2024-07-28 18:15:17 +0530

    Add files via upload

---
+++ portfolio/js web.js
@@ -0,0 +1,7 @@
+var typed= new typed('#element', {
+    Strings:['<i>Frontend Developer </i>' , '<i>Web Developer</i>'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true,
+});
